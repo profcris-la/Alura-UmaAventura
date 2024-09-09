6947a46259d3b5f485777e2164ef4fdfdc45edb9
profcris-la <crislainelima@prof.educacao.sp.gov.br>
2024-09-09 16:27:35 -0300

    acrescentando passos e imagens

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const avanca = document.querySelectorAll('.btn-proximo');
+
 avanca.forEach(button =>{
     button.addEventListener('click', function(){
-        const atual - document.querySelector('.ativo');
+        const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
         atual.classList.remove('ativo');
